1fbc070eff695ea416bf1e13eac3b994ce5cef41
SShaikh45 <22SShaikh@heckgrammar.co.uk>
2025-05-12 17:59:31 +0100

    Update Program.cs

--- LinearSearchFruitsTask/Program.cs
+++ LinearSearchFruitsTask/Program.cs
@@ -5,37 +5,33 @@
         static void Main(string[] args)
         {
                 bool found;
-    string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
-    Console.WriteLine("What would you like to find");
-    string target = Console.ReadLine();
+                string[] fruits = { "banana", "apple", "orange", "pear", "grape", "pineapple" };
+                Console.WriteLine("What would you like to find");
+                string target = Console.ReadLine();
 
-for (int i = 0; i < fruits.Length; i++)
-{
-    if (fruits[i] == target)
-    {
-        Console.WriteLine($"Your target has been found at index {i}");
-        found = true;
-        if (found == true)
-        {
-            Console.WriteLine("true");
-        }
-        else if (found == false)
-        {
-            Console.WriteLine("False");
-        }
-    }
-}
+            for (int i = 0; i < fruits.Length; i++)
+            {
+                if (fruits[i] == target)
+                {
+                    found = true;
+                    if (found == true)
+                    {
+                        Console.WriteLine("true");
+                    }
+                    else if (found == false)
+                    {
+                        Console.WriteLine("False");
+                    }
+                }
+            }
+                // Your program should:
+                // Ask the user what word they would like to find
+                // Output the message True if the word is found
+                // Output the message False if the word is not found
 
+                // You must write your own linear search routine and not use any built-in fuctions available in C#
+                // You should use meaningful variable names
 
-            
-          
-            // Your program should:
-            // Ask the user what word they would like to find
-            // Output the message True if the word is found
-            // Output the message False if the word is not found
-            
-            // You must write your own linear search routine and not use any built-in fuctions available in C#
-            // You should use meaningful variable names
         }
     }
 }
